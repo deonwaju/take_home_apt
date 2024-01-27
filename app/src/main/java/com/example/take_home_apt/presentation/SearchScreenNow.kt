1fe6ee6bab6dedfa9a177f47b61aab444c7e0e9e
Deon Olarewaju <Deonolarewaju@gmail.com>
2024-01-27 18:42:33 +0100

    working on search screen

--- app/src/main/java/com/example/take_home_apt/presentation/SearchScreenNow.kt
+++ app/src/main/java/com/example/take_home_apt/presentation/SearchScreenNow.kt
@@ -2,8 +2,6 @@ package com.example.take_home_apt.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,12 +23,13 @@ import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,29 +47,21 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.take_home_apt.R
-import com.example.take_home_apt.presentation.components.searchBar
 import com.example.take_home_apt.presentation.models.ShippingItems
 import com.example.take_home_apt.utils.Dimens
 import com.example.take_home_apt.utils.Dimens.ExtraSmallPadding
 import com.example.take_home_apt.utils.Dimens.SmallPadding1
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreenNow(
     modifier: Modifier = Modifier,
     onClick: (() -> Unit)? = null,
 ) {
+
     var searchText by remember { mutableStateOf("") }
     var searchResults by remember { mutableStateOf(emptyList<ShippingItems>()) }
-
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    val isClicked = interactionSource.collectIsPressedAsState().value
-    LaunchedEffect(key1 = isClicked) {
-        if (isClicked) {
-            onClick?.invoke()
-        }
-    }
 
     Column(
         modifier = modifier
@@ -90,7 +81,8 @@ fun SearchScreenNow(
                     .clip(MaterialTheme.shapes.medium)
                     .padding(end = Dimens.ExtraSmallPadding2)
             )
-            OutlinedTextField(
+
+            TextField(
                 value = searchText,
                 onValueChange = {
                     searchText = it
@@ -99,11 +91,21 @@ fun SearchScreenNow(
                 label = { Text("Search") },
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-                    .searchBar(),
+                    .border(
+                        width = 1.dp,
+                        color = Color.Black,
+                        shape = MaterialTheme.shapes.extraLarge
+                    ),
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
+                placeholder = {
+                    Text(
+                        text = "Search",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = colorResource(id = R.color.placeholder)
+                    )
+                },
                 leadingIcon = {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_search),
@@ -120,7 +122,18 @@ fun SearchScreenNow(
                             .size(Dimens.IconSizeMedium2)
                             .clip(CircleShape),
                     )
-                }
+                },
+                shape = MaterialTheme.shapes.medium,
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = colorResource(id = R.color.input_background),
+                    focusedTextColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                    cursorColor = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                ),
+                singleLine = true,
             )
         }
 
@@ -248,30 +261,62 @@ fun DetailsWidget(
 fun getMatchingResults(query: String): List<ShippingItems> {
     // Hardcoded data for demonstration
     val data = listOf(
-        ShippingItems("Macbook pro M2", "#NE43857340857904 • Paris → Morocco", R.drawable.ic_box_2),
+        ShippingItems(
+            name = "Macbook pro M2",
+            code = "#NE43857340857904",
+            cityFrom = "Paris",
+            cityTo = "Morocco",
+            icon = R.drawable.ic_box_2
+        ),
         ShippingItems(
             "Summer linen jacket",
-            "#NE20089934122231 • Barcelona → Paris",
+            "#NE20089934122231",
+            "Barcelona",
+            "Paris",
             R.drawable.ic_box_2
         ),
         ShippingItems(
             "Tapered fit jeans AW",
-            "#NE438343340857904 • Colombia → Paris",
+            "#NE438343340857904",
+            "Colombia",
+            "Paris",
             R.drawable.ic_box_2
         ),
-        ShippingItems("Slim fit jeans AW", "#NE848467357904 • Bogota → Dhaka", R.drawable.ic_box_2),
+        ShippingItems(
+            "Slim fit jeans AW",
+            "#NE848467357904",
+            "Bogota",
+            "Dhaka",
+            R.drawable.ic_box_2
+        ),
         ShippingItems(
             "Office setup desk",
-            "#NEHJKD57340857904 • Dubai → Dublin",
+            "#NEHJKD57340857904",
+            "Dubai",
+            "Dublin",
             R.drawable.ic_box_2
         ),
-        ShippingItems("Macbook air M2", "#NE4334GHJD40857904 • Lagos → Accra", R.drawable.ic_box_2),
+        ShippingItems(
+            "Macbook air M2",
+            "#NE4334GHJD40857904",
+            "Lagos",
+            "Accra",
+            R.drawable.ic_box_2
+        ),
         ShippingItems(
             "Dell Alien ware",
-            "#NE43857340857904 • Bali → Seychelles",
+            "#NE43857340857904",
+            "Bali",
+            "Seychelles",
             R.drawable.ic_box_2
         ),
-        ShippingItems("Hp Elite book", "#NE43857340857904 • Paris → Morocco", R.drawable.ic_box_2),
+        ShippingItems(
+            "Hp Elite book",
+            "#NE43857340857904",
+            "Paris",
+            "Morocco",
+            R.drawable.ic_box_2
+        )
     )
 
     return data.map {
--- app/src/main/java/com/example/take_home_apt/presentation/models/ShippingItems.kt
+++ app/src/main/java/com/example/take_home_apt/presentation/models/ShippingItems.kt
@@ -2,6 +2,8 @@ package com.example.take_home_apt.presentation.models
 
 data class ShippingItems(
     var name: String,
-    var details: String,
+    var code: String,
+    var cityFrom: String,
+    var cityTo: String,
     var icon: Int,
 )

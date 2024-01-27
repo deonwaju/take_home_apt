df34a10a72d3199b4eaa963786c050e9cefc8d5a
Deon Olarewaju <Deonolarewaju@gmail.com>
2024-01-27 15:51:01 +0100

    working on search screen

--- app/src/main/java/com/example/take_home_apt/MainActivity.kt
+++ app/src/main/java/com/example/take_home_apt/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.Modifier
-import com.example.take_home_apt.presentation.components.navGraph.NavGraph
-import com.example.take_home_apt.presentation.components.navGraph.Route
+import com.example.take_home_apt.presentation.SearchScreenNow
 import com.example.take_home_apt.ui.theme.Take_home_aptTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +13,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Take_home_aptTheme {
-                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    NavGraph(startDestination = Route.ShipmentNavigation.route)
-                }
+                SearchScreenNow()
+//                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+//                    NavGraph(startDestination = Route.ShipmentNavigation.route)
+//                }
             }
         }
     }
---
+++ app/src/main/java/com/example/take_home_apt/presentation/models/ShippingItems.kt
@@ -0,0 +1,7 @@
+package com.example.take_home_apt.presentation.models
+
+data class ShippingItems(
+    var name: String,
+    var details: String,
+    var icon: String,
+)

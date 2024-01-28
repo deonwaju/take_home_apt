34c4c9ce245280c3caa19cddc92595e484261f9e
Deon Olarewaju <Deonolarewaju@gmail.com>
2024-01-28 13:12:00 +0100

    working on CalculateScreen.kt

--- app/src/main/java/com/example/take_home_apt/presentation/home/CalculateScreen.kt
+++ app/src/main/java/com/example/take_home_apt/presentation/home/CalculateScreen.kt
@@ -1,6 +1,8 @@
 package com.example.take_home_apt.presentation.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,12 +17,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,13 +37,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.take_home_apt.R
 import com.example.take_home_apt.presentation.components.ToolBarComponent
 import com.example.take_home_apt.utils.Dimens
+import com.example.take_home_apt.utils.Dimens.IconSizeMedium
 import com.example.take_home_apt.utils.Dimens.SmallPadding
 import com.example.take_home_apt.utils.Dimens.SmallPadding1
 
@@ -52,7 +63,13 @@ fun CalculateScreen(
             title = "Calculate",
             onClick = {}
         )
-        DestinationSection()
+        Column(
+            modifier = modifier.padding(SmallPadding1)
+        ) {
+            DestinationSection()
+            Spacer(modifier = Modifier.height(Dimens.SmallPadding1))
+            PackagingSection()
+        }
     }
 }
 
@@ -61,9 +78,16 @@ fun DestinationSection(modifier: Modifier = Modifier) {
 
     var text by remember { mutableStateOf("") }
 
+    Text(
+        text = "Destination",
+        style = MaterialTheme.typography.titleMedium,
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier.padding(vertical = SmallPadding1)
+    )
+
     Card(
-        modifier = modifier
-            .padding(SmallPadding1),
+        modifier = modifier,
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         )
@@ -73,7 +97,7 @@ fun DestinationSection(modifier: Modifier = Modifier) {
         ) {
             TextFieldWithIconAndDivider(
                 hintText = "Sender location",
-                icon = R.drawable.ic_calculate,
+                icon = R.drawable.grey_send,
                 onValueChange = {
                     text = it
                 },
@@ -82,7 +106,7 @@ fun DestinationSection(modifier: Modifier = Modifier) {
 
             TextFieldWithIconAndDivider(
                 hintText = "Receiver location",
-                icon = R.drawable.ic_calculate,
+                icon = R.drawable.grey_receive,
                 onValueChange = {
                     text = it
                 },
@@ -91,7 +115,7 @@ fun DestinationSection(modifier: Modifier = Modifier) {
 
             TextFieldWithIconAndDivider(
                 hintText = "Approx weight",
-                icon = R.drawable.ic_calculate,
+                icon = R.drawable.hourglass,
                 onValueChange = {
                     text = it
                 },
@@ -102,10 +126,75 @@ fun DestinationSection(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun PackagingSection(modifier: Modifier = Modifier) {
+    var selectedItem by remember { mutableStateOf("Item 1") }
+    var items by remember {
+        mutableStateOf(listOf("Item 1", "Item 2", "Item 3"),)
+    }
+
+    Column {
+        Text(
+            text = "Packaging",
+            style = MaterialTheme.typography.titleMedium,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "What are you sending?",
+            style = MaterialTheme.typography.titleMedium,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Gray,
+        )
+
+        Card(
+            modifier = modifier,
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
+            Column(
+                modifier = modifier.padding(SmallPadding)
+            ) {
+                SpinnerField(
+                    hintText = "Select an item",
+                    items = items,
+                    selectedItem = selectedItem,
+                    onItemSelected = {
+                        selectedItem = it
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoriesSection(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Packaging",
+            style = MaterialTheme.typography.titleMedium,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "What are you sending?",
+            style = MaterialTheme.typography.titleMedium,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Gray,
+        )
+
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun CalculateScreenPreview() {
     Column(
     ) {
+        var selectedItem by remember { mutableStateOf("Item 1") }
         CalculateScreen()
     }
 }
@@ -137,17 +226,16 @@ fun TextFieldWithIconAndDivider(
             horizontalArrangement = Arrangement.Absolute.Center
         ) {
             // Icon on the far left
-            Icon(
+            Image(
                 painter = painterResource(id = icon),
                 contentDescription = null,
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.size(24.dp)
+                modifier = Modifier.size(IconSizeMedium)
             )
 
             Spacer(modifier = Modifier.width(16.dp))
 
             Divider(
-                color = Color.Gray,
+                color = Color.LightGray,
                 modifier = Modifier
                     .fillMaxHeight()
                     .width(1.dp)
@@ -162,7 +250,7 @@ fun TextFieldWithIconAndDivider(
                     onValueChange(it)
                 },
                 textStyle = MaterialTheme.typography.bodyMedium.copy(
-                    color = if(text.isEmpty()) Color.Gray else Color.Black,
+                    color = if (text.isEmpty()) Color.Gray else Color.Black,
                     fontFamily = FontFamily.SansSerif
                 ),
                 keyboardOptions = KeyboardOptions.Default.copy(
@@ -173,6 +261,102 @@ fun TextFieldWithIconAndDivider(
                     .fillMaxHeight(),
                 singleLine = true,
             )
+        }
+    }
+}
+
+
+@Composable
+fun SpinnerField(
+    modifier: Modifier = Modifier,
+    hintText: String,
+    items: List<String>,
+    selectedItem: String,
+    onItemSelected: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .background(MaterialTheme.colorScheme.surface) // Set gray background for the spinner field
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Icon on the far left
+
+            Image(
+                painter = painterResource(id = R.drawable.grey_box),
+                contentDescription = null,
+                modifier = Modifier.size(IconSizeMedium)
+            )
+
+            // Spacer to separate icon and divider
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Vertical Divider
+            Divider(
+                color = Color.LightGray,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(1.dp)
+            )
+
+            // Spacer to separate divider and text input
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Text input (read-only)
+            BasicTextField(
+                value = selectedItem,
+                onValueChange = {},
+                textStyle = MaterialTheme.typography.bodyMedium.copy(
+                    color = MaterialTheme.colorScheme.onSurface
+                ),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                readOnly = true,
+                modifier = Modifier
+                    .weight(1f) // Takes up remaining space
+                    .fillMaxHeight()
+                    .clickable {
+                        expanded = true
+                    }
+            )
+
+            // Dropdown Icon
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = stringResource(id = R.string.cd_dropdown_icon),
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(IconSizeMedium)
+            )
+        }
+
+        // Dropdown Menu
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            text = item,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(SmallPadding)
+                        )
+                    },
+                    onClick = {
+                        onItemSelected(item)
+                        expanded = false
+                    }
+                )
+            }
         }
     }
 }

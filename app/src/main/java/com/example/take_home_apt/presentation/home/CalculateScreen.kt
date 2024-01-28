4ff321f8d1cf13a1591b1b7ee7466fb40ee777de
Deon Olarewaju <Deonolarewaju@gmail.com>
2024-01-28 02:52:56 +0100

    working on CalculateScreen.kt

--- app/src/main/java/com/example/take_home_apt/presentation/home/CalculateScreen.kt
+++ app/src/main/java/com/example/take_home_apt/presentation/home/CalculateScreen.kt
@@ -2,23 +2,19 @@ package com.example.take_home_apt.presentation.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.take_home_apt.utils.Dimens
 
@@ -27,7 +23,9 @@ fun CalculateScreen(
     modifier: Modifier = Modifier,
     name: String = "CalculateScreen"
 ) {
-    AppBarScreen(onClick = {})
+    Column {
+        AppBarScreen(onClick = {})
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,42 +33,33 @@ fun CalculateScreen(
 fun AppBarScreen(
     onClick: (() -> Unit)? = null,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        TopAppBar(
-            title = {
+    CenterAlignedTopAppBar(
+        title = {
 
-                Row {
-                    Icon(
-                        imageVector = Icons.Default.KeyboardArrowLeft,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(Dimens.IconSizeMedium2)
-                            .clip(MaterialTheme.shapes.medium)
-                            .clickable {
-                                onClick?.invoke()
-                            },
-                        tint = Color.White
-                    )
-                    Text(
-                        text = "Page Name",
-                        color = Color.White,
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
-                }
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primary
+            Text(
+                text = "Calculate",
+                color = Color.White,
+                style = MaterialTheme.typography.titleLarge,
             )
-        )
 
-        // Rest of your screen content goes here
-    }
+        },
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowLeft,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(Dimens.IconSizeMedium2)
+                    .clip(MaterialTheme.shapes.medium)
+                    .clickable {
+                        onClick?.invoke()
+                    },
+                tint = Color.White
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
 }
 
 @Composable
